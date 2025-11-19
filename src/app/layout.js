09bf3977086e0d1462c09cb89b15e8a9dc77286d
Omar Chakroun <omar.chakroun@impactgroup.tech>
2025-11-19 13:34:48 +0100

    CHZ-19: Create NavBar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,15 @@
+import Navbar from "../components/navbar";
+
+export const metadata = {
+  title: "Admin Dashboard",
+};
 export default function RootLayout({ children }) {
   return (
-    <html>
+    <html lang="en">
       <body>
-        {children}
+        <Navbar />
+        <main>{children}</main>
+        {/* <Footer /> */}
       </body>
     </html>
   );
